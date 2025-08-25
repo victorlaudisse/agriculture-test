e4f9489ec9349b07d576c2e383b482b5a142154e
victorlaudisse <victorlaudissebueno@gmail.com>
2025-08-25 20:39:40 -0300

    fix(backend): adjust relative path in Jest config

---
+++ apps/backend/jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "jest";
+
+const config: Config = {
+  rootDir: "./",
+  modulePaths: ["<rootDir>"],
+  displayName: "backend",
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/?(*.)+(spec|test).ts"],
+  moduleFileExtensions: ["ts", "js", "json"],
+  setupFiles: ["dotenv/config"],
+  clearMocks: true,
+  coverageDirectory: "<rootDir>/coverage",
+  collectCoverageFrom: ["src/**/*.(t|j)s", "!src/main.ts", "!src/**/index.ts"],
+};
+
+export default config;
