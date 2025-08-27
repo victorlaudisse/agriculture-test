c9d520d2e0574924ebb997b9d74e59482221c29d
victorlaudisse <victorlaudissebueno@gmail.com>
2025-08-27 03:34:28 -0300

    style(backend): lint files

--- apps/backend/src/activities/activities.module.ts
+++ apps/backend/src/activities/activities.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
-import { ActivitiesController } from './activities.controller';
-import { ActivitiesService } from './activities.service';
+import { Module } from "@nestjs/common";
+
+import { ActivitiesController } from "./activities.controller";
+import { ActivitiesService } from "./activities.service";
 
 @Module({
   controllers: [ActivitiesController],
-  providers: [ActivitiesService]
+  providers: [ActivitiesService],
 })
 export class ActivitiesModule {}
